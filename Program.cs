7f55a747c1181aab3c846ff822c0afb717612d62
PetirrzZzC <ananda_prasetyo_34pg@student.smktelkom-mlg.sch.id>
2026-01-14 09:10:18 +0700

    Parameter dan Argument
    Menambahkan Parameter dan Argument

--- Program.cs
+++ Program.cs
@@ -1,40 +1,47 @@
 ﻿
-static void tambah()
+static void tambah(int a, int b)
 {
-    int a = 3, b = 2, c = 0;
+    int c = 0;
 
     c = a + b;
     Console.WriteLine ($"{a} + {b} = {c}");
 }
 
 
-static void kurang()
+static void kurang(int a, int b)
 {
-    int a = 3, b = 2, c = 0;
+    int c = 0;
 
     c = a - b;
     Console.WriteLine($"{a} - {b} = {c}");
 }
 
 
-static void kali()
+static void kali(int a, int b)
 {
-    int a = 3, b = 2, c = 0;
+    int c = 0;
 
     c = a * b;
     Console.WriteLine($"{a} * {b} = {c}");
 }
 
 
-static void bagi()
+static void bagi(int a, int b)
 {
-    int a = 3, b = 2, c = 0;
+    int c = 0;
 
     c = a / b;
     Console.WriteLine($"{a} / {b} = {c}");
 }
 
-tambah();
-kurang();
-kali();
-bagi();
+tambah(5,7);
+tambah(5,9);
+
+kurang(8,3);
+kurang(12, 5);
+
+kali(7,4);
+kali(8,7);
+
+bagi(12,2);
+bagi(15, 3);
