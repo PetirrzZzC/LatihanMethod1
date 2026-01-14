11d3a5f275f465e5eed1c2ec22691a952d4cb4d8
PetirrzZzC <ananda_prasetyo_34pg@student.smktelkom-mlg.sch.id>
2026-01-14 08:53:38 +0700

    Belajar Method
    Membuat method tambah, kurang, kali, dan bagi.

--- Program.cs
+++ Program.cs
@@ -1,2 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+static void tambah()
+{
+    int a = 3, b = 2, c = 0;
+
+    c = a + b;
+    Console.WriteLine ($"{a} + {b} = {c}");
+}
+
+
+static void kurang()
+{
+    int a = 3, b = 2, c = 0;
+
+    c = a - b;
+    Console.WriteLine($"{a} - {b} = {c}");
+}
+
+
+static void kali()
+{
+    int a = 3, b = 2, c = 0;
+
+    c = a * b;
+    Console.WriteLine($"{a} * {b} = {c}");
+}
+
+
+static void bagi()
+{
+    int a = 3, b = 2, c = 0;
+
+    c = a / b;
+    Console.WriteLine($"{a} / {b} = {c}");
+}
+
+tambah();
+kurang();
+kali();
+bagi();
